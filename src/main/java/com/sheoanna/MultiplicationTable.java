213823fd35b8a274130405e752bb65601551af0f
NepyAnna <nepyann@gmail.com>
2024-11-11 10:02:59 +0100

    add tests to class MultiplicationTable final class App

--- src/main/java/com/sheoanna/MultiplicationTable.java
+++ src/main/java/com/sheoanna/MultiplicationTable.java
@@ -2,7 +2,6 @@ package com.sheoanna;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class MultiplicationTable {
 
--- src/test/java/com/sheoanna/AppTest.java
+++ src/test/java/com/sheoanna/AppTest.java
@@ -1,18 +1,44 @@
 package com.sheoanna;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    
     @Test
     void testApp() {
-        assertEquals(1, 1);
+        String simulatedInput = "5\n";
+
+        PrintStream originalOut = System.out;
+        InputStream originalIn = System.in;
+
+        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(input);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        App.main(new String[]{});
+
+        String expectedOutput = "Put your number: \n" + 
+                                "5 x 1 = 5\n" +
+                                "5 x 2 = 10\n" +
+                                "5 x 3 = 15\n" +
+                                "5 x 4 = 20\n" +
+                                "5 x 5 = 25\n" +
+                                "5 x 6 = 30\n" +
+                                "5 x 7 = 35\n" +
+                                "5 x 8 = 40\n" +
+                                "5 x 9 = 45\n" +
+                                "5 x 10 = 50\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+
+        System.setOut(originalOut);
+        System.setIn(originalIn);
     }
 }
